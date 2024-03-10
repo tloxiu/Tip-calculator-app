72fd70ffcf0083b431c2b82e2b40c36a0e6392ee
tloxiu <153999211+tloxiu@users.noreply.github.com>
2024-03-10 07:29:21 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX).
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](/design/desktop-preview.jpg)
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+
+## Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@tloxiu](https://www.frontendmentor.io/profile/tloxiu)
+
---
+++ script.js
@@ -0,0 +1,87 @@
+const inputBill = document.querySelector(".input-bill");
+const optionButtons = document.querySelectorAll(".option-btn");
+const inputPeopleNumber = document.querySelector(".input-people-number");
+const inputCustomTip = document.querySelector(".input-custom-tip")
+const errorInfo = document.querySelector(".error");
+const tipPerPerson = document.querySelector(".tip-amount-value");
+const tipTotalPerPerson = document.querySelector(".tip-total-value");
+const resetButton = document.querySelector(".reset-btn");
+
+function areValuesValid(){
+    if(inputPeopleNumber.value === "0"){
+        errorInfo.style.opacity = "1";
+        errorInfo.innerHTML = `Can't be zero`
+        return;
+    }
+
+    return (inputBill.value !== "" && inputPeopleNumber.value !== "") ? true : false;
+}
+
+function showResults(tip, tipTotal) {
+    tipPerPerson.style.opacity = "1";
+    tipTotalPerPerson.style.opacity = "1";
+    tipPerPerson.innerHTML = `$${tip}`;
+    tipTotalPerPerson.innerHTML = `$${tipTotal}`;
+};
+
+function calculateTip(selectedPercentage){
+    let selectedPercentageString = String(selectedPercentage);
+    let trimmedPercentage = selectedPercentageString.slice(0, -1) / 100;
+    let finalPercentage = parseFloat(trimmedPercentage);
+
+    let resultTipPerPerson = (inputBill.value * finalPercentage) / inputPeopleNumber.value;
+    let resultTipTotalPerPerson = (inputBill.value * finalPercentage / inputPeopleNumber.value) + (inputBill.value / inputPeopleNumber.value);
+
+    showResults(resultTipPerPerson, resultTipTotalPerPerson);
+};
+
+function calculateCustomTip(percentage){
+    let finalPercentage = percentage / 100;
+    let resultTipPerPerson = (inputBill.value * finalPercentage) / inputPeopleNumber.value;
+    let resultTipTotalPerPerson = (inputBill.value * finalPercentage / inputPeopleNumber.value) + (inputBill.value / inputPeopleNumber.value);
+
+    showResults(resultTipPerPerson, resultTipTotalPerPerson);
+};
+
+
+function resetValues(){
+    inputBill.value = "";
+    inputPeopleNumber.value = "";
+    inputCustomTip.value = "";
+    tipPerPerson.textContent = "";
+    tipTotalPerPerson.textContent = "";
+};
+
+optionButtons.forEach(button => {
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let result = areValuesValid();
+
+        if(result){
+            resetButton.classList.remove("disabled")
+            calculateTip(button.textContent);
+        }
+        else if(!result){
+            resetButton.classList.add("disabled")
+            return;
+        }
+    });
+});
+
+resetButton.addEventListener("click", resetValues);
+
+inputCustomTip.addEventListener('input', function(e){
+
+    let userTipValue = e.target.value;
+    let result = areValuesValid();
+
+    if(result){
+        resetButton.classList.remove("default-btn")
+        calculateCustomTip(userTipValue)
+    }
+    else if(!result){
+        resetButton.classList.add("default-btn")
+        return;
+    }
+});
